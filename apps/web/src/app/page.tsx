4504ea2712f125a2721fef5d0fcf9eb367b34a1b
Camilo Rojas <camilo.rojas@medismart.live>
2023-12-28 18:25:09 -0300

    changes

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,12 +1,10 @@
 export default function Page(): JSX.Element {
   return (
-    <main>
-      <iframe src="https://medical.medismart.live" height="100%" width="100%" frameBorder="0" title="My Page">
-        Your browser does not support iframes.
-        <a href="https://medical.medismart.live" target="_blank" rel="noopener noreferrer">
-          Click here to view the page directly.
-        </a>
-      </iframe>
-    </main>
+    <iframe src="https://medical.medismart.live" height="100%" width="100%" frameBorder="0" title="My Page">
+      Your browser does not support iframes.
+      <a href="https://medical.medismart.live" target="_blank" rel="noopener noreferrer">
+        Click here to view the page directly.
+      </a>
+    </iframe>
   );
 }
